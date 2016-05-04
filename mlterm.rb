9d2170cbbe55157ade0a6e7dd1b5063de1af98b5
Hayaki Saito <user@zuse.jp>
2016-05-04 15:36:12 +0900

    Add patch for X11 build to strip CoreFoundation dependency

--- mlterm.rb
+++ mlterm.rb
@@ -7,6 +7,10 @@ class Mlterm < Formula
   depends_on "pkg-config" => :build
   depends_on :x11 => :optional
 
+  stable do
+    patch :DATA
+  end
+
   def install
     args = ["--prefix=#{prefix}",
            ]
@@ -37,3 +41,44 @@ class Mlterm < Formula
   end
 
 end
+
+__END__
+diff -r 61224c609927 mlterm/ml_char.c
+--- a/mlterm/ml_char.c	Thu Apr 28 02:09:38 2016 +0900
++++ b/mlterm/ml_char.c	Sat Apr 30 03:59:42 2016 +0900
+@@ -189,7 +189,7 @@
+ 	return  multi_ch + comb_size ;
+ }
+ 
+-#ifdef  __APPLE__
++#ifdef  USE_QUARTZ
+ static void
+ normalize(
+ 	ml_char_t *  ch ,
+@@ -465,7 +465,7 @@
+ 	}
+ }
+ 
+-#ifdef  __APPLE__
++#ifdef  USE_QUARTZ
+ #include  <kiklib/kik_mem.h>
+ int  ml_normalize( u_int16_t *  str , int  num) ;
+ #endif
+@@ -521,7 +521,7 @@
+ 		return  NULL ;
+ 	}
+ 
+-#ifdef  __APPLE__
++#ifdef  USE_QUARTZ
+ 	normalize( ch , comb_size) ;
+ #endif
+ 
+@@ -557,7 +557,7 @@
+ 	*comb = *src ;
+ 	UNSET_COMB_TRAILING(comb->u.ch.attr) ;
+ 
+-#ifdef  __APPLE__
++#ifdef  USE_QUARTZ
+ 	normalize( ch , comb_size) ;
+ #endif
+ 
